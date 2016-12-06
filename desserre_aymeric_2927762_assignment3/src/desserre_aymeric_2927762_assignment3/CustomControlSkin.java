8b87039722dfdaeaba3a25b11f03cecf5d0a2495
Aymeric <Aymeric@DESKTOP-JDJANRG>
2016-12-06 11:58:11 +0000

    git commit board

---
+++ desserre_aymeric_2927762_assignment3/src/desserre_aymeric_2927762_assignment3/CustomControlSkin.java
@@ -0,0 +1,13 @@
+package desserre_aymeric_2927762_assignment3;
+
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
+
+public class CustomControlSkin extends SkinBase<CustomControl> implements Skin<CustomControl> {
+
+	protected CustomControlSkin(CustomControl cc) {
+		super(cc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
